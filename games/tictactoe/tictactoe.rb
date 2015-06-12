6efd16a91a1110ade66858cd1d5c9b1d0d907bb3
Omar S <osanseviero@gmail.com>
2015-06-11 23:13:21 -0500

    Adding win conditions and updating the board

--- games/tictactoe/tictactoe.rb
+++ games/tictactoe/tictactoe.rb
@@ -11,12 +11,15 @@
 # Has all the game logic
 class Game
 	attr_reader :board, :p1, :p2
+	@@player = 0
+
 
 	# Creates the players and the grid
 	def initialize
 		@board = Board.new
 		@p1 = Player.new
 		@p2 = Player.new
+		players_data
 	end
 
 	# Prints players data
@@ -24,6 +27,27 @@ class Game
 		puts @p1.data
 		puts @p2.data
 	end
+
+	# This is a turn
+	def update
+
+		@board.print_grid
+		puts "What row do you want to update?"
+		row = gets.chomp.to_i
+		puts "What column do you want to update?"
+		column = gets.chomp.to_i
+		if @@player == 0
+			@board.update(row, column, @p1.sym)
+			@board.print_grid
+			@board.checkWin('X')
+		else
+			@board.update(row, column, @p2.sym)
+			@board.print_grid
+			@board.checkWin('O')
+		end
+	end
+
+
 
 
 	# Holds data for the players
@@ -69,17 +93,41 @@ class Game
 
 		#Updates the cell
 		def update(row, cell, sym)
-			if @board[row][cell] != @empty_cell
-				puts "This cell has something, try again!"
-				return false
-			else	
-				@board[row][cell] = sym
-				return true
+			check = false
+			while !check
+				if @board[row-1][cell-1] != @empty_cell
+					puts "This cell has something, try again! Put another row and column"
+					puts "What row do you want to update?"
+					row = gets.chomp.to_i
+					puts "What column do you want to update?"
+					cell = gets.chomp.to_i
+					check = false
+				else	
+					@board[row-1][cell-1] = sym
+					check = true
+				end
 			end
+		end
+
+		# Checks if a player has won
+		def checkWin(sym)
+			if board[0].all? {|cell| cell.to_s == sym}
+				puts "FINISH"
+			else board[1].all? {|cell| cell.to_s == sym}
+				puts "FINISH"
+			else board[2].all? {|cell| cell.to_s == sym}
+				puts "FINISH"
+			end
+
 		end
 
 	end
 end
 
 a = Game.new
-a.players_data
+a.update
+a.update
+a.update
+a.update
+a.update
+a.update
