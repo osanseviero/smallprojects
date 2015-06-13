7f80a1ec9340ebdc77c5d61d5407a6e9b79bb212
Omar S <osanseviero@gmail.com>
2015-06-13 09:11:16 -0500

    Added color guessing and validation

--- games/mastermind/mastermind.rb
+++ games/mastermind/mastermind.rb
@@ -4,18 +4,47 @@
 
 # Holds all the game logic
 class Game
-	attr_accessor :code
+	attr_accessor :code, :guess
 
 	def initialize
+		@guess = []
 		generateCode
-		#Player guess code
-		# Computer compares and gives results
+		pguessCode
+		checkGuess
 	end
 
+	# Computer generates a random code
 	def generateCode
 		options = ['R','G','B','Y','W','Bl']
 		@code = options.sample(4)
-		puts @code.is_a? Array 
+		puts @code
+	end
+
+	# Player guess the code
+	def pguessCode
+		puts "The options are: R/G/B/Y/W/BL"
+		counter = 1
+		4.times {
+			puts "Enter color at position #{counter}"
+			input = gets.chomp
+			guess << input
+			counter +=1
+		}
+	end
+
+	# Computer checks the code
+	def checkGuess
+		correct = 0
+		position = 0
+		guess.each_with_index{ |element, index|
+			if element == code[index]
+				correct += 1
+			elsif code.include?(element)
+				position += 1
+			end
+		}
+		puts "Correct position " + correct.to_s
+		puts "Color in wrong position " + position.to_s 
 	end
 
 end
