14dc8ed415dd0858677db5a6f2aaeb1133c3201c
Omar S <osanseviero@gmail.com>
2015-06-13 19:04:29 -0500

    Adding dummy text

---
+++ textAnalyzer/textAnalyzer.rb
@@ -0,0 +1,6 @@
+# This text analyzer has the next features
+	# Character count with and without spaces
+	# Line count
+	# Word count
+	# Sentence and paragraph count
+	# Avg of words per sentence and sentences per paragraph
