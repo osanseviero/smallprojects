32d8c555a51e89a921865428b606fbeb44b2cd12
Omar S <osanseviero@gmail.com>
2015-06-14 16:52:03 -0500

    Zipcode filter

--- eventManager/lib/event_manager.rb
+++ eventManager/lib/event_manager.rb
@@ -1,10 +1,22 @@
 require "csv"
 
+def clean_zipcode(zipcode)
+	if zipcode.nil?
+		zipcode = '00000'
+	elsif zipcode.length < 5
+    zipcode = zipcode.rjust 5, "0"
+  elsif zipcode.length > 5
+    zipcode = zipcode[0..4]
+  end
+end
+
+
 puts 'EventManger initialized'
 
 contents = CSV.open "../event_atendees.csv", headers:true, header_converters: :symbol
 contents.each{ |row|
 	name = row[:first_name]
-	zipcode = row[:zipcode]
+	zipcode = clean_zipcode(row[:zipcode])
 	puts name + " " + zipcode.to_s
 }
+
