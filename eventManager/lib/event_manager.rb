bad130c2e64d004dab765e4742eab3740737a8f2
Omar S <osanseviero@gmail.com>
2015-06-14 17:24:03 -0500

    Now gives the list of legislators with Sunlight Congress API

--- eventManager/lib/event_manager.rb
+++ eventManager/lib/event_manager.rb
@@ -1,7 +1,17 @@
 require "csv"
+require "sunlight/congress"
+
+Sunlight::Congress.api_key = "e179a6973728c4dd3fb1204283aaccb5"
 
 def clean_zipcode(zipcode)
 	zipcode.to_s.rjust(5,"0")[0..4]
+end
+
+def legislators_by_zipcode(zipcode)
+	legislators = Sunlight::Congress::Legislator.by_zipcode(zipcode)
+	legislator_names = legislators.collect{ |legislator|
+		"#{legislator.first_name} #{legislator.last_name}"
+	}
 end
 
 
@@ -11,6 +21,7 @@ contents = CSV.open "../event_atendees.csv", headers:true, header_converters: :s
 contents.each{ |row|
 	name = row[:first_name]
 	zipcode = clean_zipcode(row[:zipcode])
-	puts name + " " + zipcode.to_s
+	legislators = legislators_by_zipcode(zipcode)
+	puts "#{name} #{zipcode} #{legislators}"
 }
 
