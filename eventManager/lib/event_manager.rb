6d3e4bcd0e58a0ae0fabf14b48cef3097d89c6db
Omar S <osanseviero@gmail.com>
2015-06-14 09:14:46 -0500

    Adding first iteration

--- eventManager/lib/event_manager.rb
+++ eventManager/lib/event_manager.rb
@@ -1,1 +1,10 @@
+require "csv"
+
 puts 'EventManger initialized'
+
+contents = CSV.open "../event_atendees.csv", headers:true, header_converters: :symbol
+contents.each{ |row|
+	name = row[:first_name]
+	zipcode = row[:zipcode]
+	puts name + " " + zipcode.to_s
+}
