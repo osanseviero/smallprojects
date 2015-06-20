476566f0fe60790a4ec4413048f063275b5fee42
Omar S <osanseviero@gmail.com>
2015-06-20 11:20:24 -0500

    Added depth first search and recursive

--- rubyproblems/binary_tree.rb
+++ rubyproblems/binary_tree.rb
@@ -43,5 +43,38 @@ def breadth_first_search(n, node)
   end
 end
 
+def depth_first_search(n, node)
+  visited = [node]
+  stack = [node]
+  while stack.size > 0
+    return stack[0] if stack[0].value == n
+    if stack[0].left && !visited.include?(stack[0].left)
+      stack.unshift(stack[0].left)
+      visited << stack[0]
+    elsif stack[0].right && !visited.include?(stack[0].right)
+      stack.unshift(stack[0].right)
+      visited << stack[0]
+    else
+      stack.shift
+    end
+  end
+end
+
+def dfs_rcs(n, node, visited = [node])
+  return node if node.value == n
+  if node.left && !visited.include?(node.left)
+    dfs_rcs(n, node.left, visited << node.left)
+  elsif node.right && !visited.include?(node.right)
+    dfs_rcs(n, node.right, visited << node.right)
+  elsif node.parent != nil
+    dfs_rcs(n, node.parent, visited)
+  else
+    nil
+  end
+end
+
+
 tree = build_tree([5,7,1,4,9,7,8,3])
-breadth_first_search(8, tree)
+p breadth_first_search(8, tree)
+p depth_first_search(8, tree)
+p depth_first_search(30, tree)
